92c0d5acf851e3a1375f6ab3149cd0309b23659b
Naitik Shah <n@daaku.org>
2009-08-15 01:09:46 +0000

    added watir based automation for tests

---
+++ tests/tests.rb
@@ -0,0 +1,20 @@
+require 'test/unit'
+require 'watir'
+
+
+class Delegator < Test::Unit::TestCase
+
+  def test_qunit
+    browser = Watir::Browser.new
+    browser.goto('http://dev.streamdiff.com/delegator/tests/index.html')
+    browser.link(:id, 'click-one').click
+    browser.link(:id, 'click-two').click
+    browser.link(:id, 'click-three').click
+    sleep 1
+    browser.text_field(:id, 'focus-four').focus
+    browser.text_field(:id, 'focus-five').focus
+    sleep 0.2
+    assert('pass' == browser.h2(:id, 'banner').attribute_value('class'))
+  end
+
+end
